6b340afb3a587b3bcc7e51932a5210316f1f10aa
Sorcerer <sagooon@outlook.com>
2022-12-24 01:06:40 +0545

    Main

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Calculator:
         self.window = tk.Tk()
         self.window.geometry("375x500")
         self.window.resizable(0, 0)
-        self.window.title("Calculator")
+        self.window.title("PyCalc")
 
         self.total_expression = ""
         self.current_expression = ""
